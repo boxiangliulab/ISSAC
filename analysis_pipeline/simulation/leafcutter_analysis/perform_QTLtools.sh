6ed862bb5954234cc2e747f65281e613e49e9498
Yuntian Zhang <40263709+tibettiger@users.noreply.github.com>
2025-08-05 15:00:30 +0800

    Update perform_QTLtools.sh

--- analysis_pipeline/simulation/leafcutter_analysis/perform_QTLtools.sh
+++ analysis_pipeline/simulation/leafcutter_analysis/perform_QTLtools.sh
@@ -4,7 +4,7 @@ sample=400
 
 /data/projects/11003054/e0950183/software/QTLtools cis --vcf /data/projects/11003054/e0950183/compare_site_intron_based/effect_size/genotype/pseudobulk_geno.vcf.gz \
                       --bed /home/users/nus/e0950183/scratch/new_simulation_result/phenotype/sample400_iso2/sample400_iso2_effect${effect}.bed.gz \
-                      --cov /data/projects/11003054/e0950183/compare_site_intron_based/effect_size/PC/effect1_5.PCs \
+                      --cov /data/projects/11003054/e0950183/compare_site_intron_based/effect_size/PC/effect${effect}.PCs \
                       --nominal 1 \
                       --normal \
                       --out /home/users/nus/e0950183/scratch/new_simulation_result/result/QTLtools/${effect}_${sample}_nominal
@@ -18,7 +18,7 @@ for effect in ${e[@]};do
     tabix -p bed /home/users/nus/e0950183/scratch/new_simulation_result/phenotype/sample400_iso2/site_based_sample400_iso2_effect${effect}.bed.gz
     /data/projects/11003054/e0950183/software/QTLtools cis --vcf /data/projects/11003054/e0950183/compare_site_intron_based/effect_size/genotype/pseudobulk_geno.vcf.gz \
                       --bed /home/users/nus/e0950183/scratch/new_simulation_result/phenotype/sample400_iso2/site_based_sample400_iso2_effect${effect}.bed.gz \
-                      --cov /data/projects/11003054/e0950183/compare_site_intron_based/effect_size/PC/effect1_5.PCs \
+                      --cov /data/projects/11003054/e0950183/compare_site_intron_based/effect_size/PC/effect${effect}.PCs \
                       --nominal 1 \
                       --normal \
                       --out /home/users/nus/e0950183/scratch/new_simulation_result/result/QTLtools/site_based_${effect}_${sample}_nominal
