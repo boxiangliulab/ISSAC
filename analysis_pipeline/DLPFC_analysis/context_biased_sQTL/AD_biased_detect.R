2b5d25537536e29e1667cfa040c01f5320bb6b05
Yuntian Zhang <40263709+tibettiger@users.noreply.github.com>
2025-09-01 14:00:13 +0800

    Update AD_biased_detect.R

--- analysis_pipeline/DLPFC_analysis/context_biased_sQTL/AD_biased_detect.R
+++ analysis_pipeline/DLPFC_analysis/context_biased_sQTL/AD_biased_detect.R
@@ -125,7 +125,7 @@ for(i in start:end){
     model <- glmer(cbind(suc, fal) ~ sPC1 + sPC2 + sPC3 + sPC4 + sPC5 + sPC6 + sPC7 + sPC8 + gPC1 + gPC2 + gPC3 + gPC4 + gPC5 + 
         sex + age + AD + ROS_MAP + PMI + num_cells + educ + data_source + geno + geno_AD+(1|group), 
                data = model_data, 
-               family = binomial("logit"))
+               family = binomial(link="logit"))
     final_result$V5[i]<-summary(model)$coefficients[,'Pr(>|z|)']['geno']
     final_result$V6[i]<-summary(model)$coefficients[,'Pr(>|z|)']['geno_AD']
     num<-which(model_data$AD==0)
@@ -133,7 +133,7 @@ for(i in start:end){
     model <- glmer(cbind(suc, fal) ~ sPC1 + sPC2 + sPC3 + sPC4 + sPC5 + sPC6 + sPC7 + sPC8 + gPC1 + gPC2 + gPC3 + gPC4 + gPC5 + 
         sex + age + ROS_MAP + PMI + num_cells + educ + data_source + geno+(1|group), 
                data = newmodel, 
-               family = binomial("logit"))
+               family = binomial(link="logit"))
     final_result$V8[i]<-summary(model)$coefficients[,'Pr(>|z|)']['geno']
     final_result$V10[i]<-computeRegressionSlope(as.numeric(pheno_revise[num]),as.numeric(newmodel$geno))
     num<-which(model_data$AD==1)
@@ -141,7 +141,7 @@ for(i in start:end){
     model <- glmer(cbind(suc, fal) ~ sPC1 + sPC2 + sPC3 + sPC4 + sPC5 + sPC6 + sPC7 + sPC8 + gPC1 + gPC2 + gPC3 + gPC4 + gPC5 + 
         sex + age + ROS_MAP + PMI + num_cells + educ + data_source + geno+(1|group), 
                data = newmodel, 
-               family = binomial("logit"))
+               family = binomial(link="logit"))
     final_result$V9[i]<-summary(model)$coefficients[,'Pr(>|z|)']['geno']
     final_result$V11[i]<-computeRegressionSlope(as.numeric(pheno_revise[num]),as.numeric(newmodel$geno))
 }
