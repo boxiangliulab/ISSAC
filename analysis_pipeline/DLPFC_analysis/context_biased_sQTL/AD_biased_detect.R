d0009163708a50998176aaf54854c62e1cc297bb
Yuntian Zhang <40263709+tibettiger@users.noreply.github.com>
2025-10-28 15:00:34 +0800

    Update AD_biased_detect.R

--- analysis_pipeline/DLPFC_analysis/context_biased_sQTL/AD_biased_detect.R
+++ analysis_pipeline/DLPFC_analysis/context_biased_sQTL/AD_biased_detect.R
@@ -31,6 +31,7 @@ computeRegressionSlope <- function(x, y) {
 }
 library(lme4)
 library(QuantPsyc)
+library(glmmTMB)
 
 dat<-read.table(paste("/home/users/nus/e0950183/scratch/brain_major_sub/result/analysis/",cell,".add_gene_sum",sep=""),sep="\t")
 
@@ -126,6 +127,11 @@ for(i in start:end){
         sex + age + AD + ROS_MAP + PMI + num_cells + educ + data_source + geno + geno_AD+(1|group), 
                data = model_data, 
                family = binomial(link="logit"))
+    ### if using glmmTMB
+    #model <- glmmTMB(cbind(suc, fal) ~ sPC1 + sPC2 + sPC3 + sPC4 + sPC5 + sPC6 + sPC7 + sPC8 + gPC1 + gPC2 + gPC3 + gPC4 + gPC5 + 
+    #    sex + age + AD + ROS_MAP + PMI + num_cells + educ + data_source + geno + geno_AD+(1|group), 
+    #           data = model_data, 
+    #           family = binomial)
     final_result$V5[i]<-summary(model)$coefficients[,'Pr(>|z|)']['geno']
     final_result$V6[i]<-summary(model)$coefficients[,'Pr(>|z|)']['geno_AD']
     num<-which(model_data$AD==0)
